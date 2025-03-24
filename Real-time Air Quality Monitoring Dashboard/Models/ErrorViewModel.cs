baf17745271d191aa3c5fd8e5cc2d8c5ec586836
Ranudi <ranudigk@gmail.com>
2025-03-25 01:14:17 +0530

    Add project files.

---
+++ Real-time Air Quality Monitoring Dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Real_time_Air_Quality_Monitoring_Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
