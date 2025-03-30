4bcc1a8861de2386c0bb691bf99c3652e17425cc
Ranudi <ranudigk@gmail.com>
2025-03-30 22:33:47 +0530

    add the pages map and about

---
+++ Real-time Air Quality Monitoring Dashboard/Controllers/MapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Real_time_Air_Quality_Monitoring_Dashboard.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
