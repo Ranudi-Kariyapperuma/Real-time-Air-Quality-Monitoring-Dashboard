e209cafd01fc8e5eb68c8a970a03342a38cd73ff
nckodikara@students.nsbm.ac.lk <nckodikara@students.nsbm.ac.lk>
2025-04-04 16:43:20 +0530

    Added a view and a controller

---
+++ Real-time Air Quality Monitoring Dashboard/Controllers/AirQualityController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+public class AirQualityController : Controller
+{
+    public IActionResult GetAqiData()
+    {
+        var aqiData = new
+        {
+            labels = new List<string>(),
+            values = new List<int>()
+        };
+
+        Random rand = new Random();
+        for (int i = 6; i >= 0; i--)
+        {
+            aqiData.labels.Add(DateTime.Now.AddDays(-i).ToString("yyyy-MM-dd"));
+            aqiData.values.Add(rand.Next(50, 200)); // Simulating AQI values
+        }
+
+        return Json(aqiData);
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
